3e16e4cd252e699eb0e72aa511223af45b625bdf
butakoma <ky.tanabota@gmail.com>
2020-04-23 23:00:51 +0900

    add jquery ui dialog

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -27,7 +27,10 @@ namespace MvcCalendar.Controllers
         {
             return View();
         }
-
+        public IActionResult About()
+        {
+            return View();
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
